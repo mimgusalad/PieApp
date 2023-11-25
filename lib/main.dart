d8c328acc1bf34cb17f2d11ee9abfbcd46590f01
kyungmin <dakjijisalad@gmail.com>
2023-11-26 06:56:06 +0900

    feat: 센드버드 계정 생성 - 존재하지 않으면 자동으로 생성되고 이메일이 아이디로, 닉네임이 닉네임으로 설정됨

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sendbird_chat_sdk/sendbird_chat_sdk.dart';
 import 'package:get/get.dart';
 import './style.dart' as style;
-import './storage.dart' as store;
+import 'Storage/review_storage.dart';
+import 'Storage/succ_storage.dart';
 import 'Pages/home.dart' as home;
 import 'Pages/bookmark.dart' as bookmark;
 import 'Pages/mypage.dart' as profile;
 import 'Pages/succ.dart' as succ;
-import 'Pages/chatList.dart' as message;
+import 'Pages/message_list.dart' as message;
 import 'Components/message.dart';
+import 'package:http/http.dart' as http;
 
 
 const APP_ID = '4E4C060D-17A6-4CC6-84EA-47D1C87A1F43';
@@ -19,18 +23,30 @@ const API_TOKEN = '1d5127c511ce2b3d1e1f27bde93727381958dc63';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   SendbirdChat.init(appId: APP_ID);
-  await SendbirdChat.connect('test_id2', accessToken: API_TOKEN);
+  // 현재 접속한 사람 userId
+  var user = await SendbirdChat.connect('유저아이디', accessToken: API_TOKEN);
+  if(user.nickname.isEmpty){
+    var res = await http.put(Uri.parse('https://api-$APP_ID.sendbird.com/v3/users/${user.userId}'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Api-Token': API_TOKEN
+        },
+        body: jsonEncode({
+          // 현재 접속한 사람 닉네임
+          'nickname': '닉네임',
+        }));
+    debugPrint(res.body);
+  }
 
   runApp(
     MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (c)=> store.UserStorage()),
-          ChangeNotifierProvider(create: (c)=>store.ReviewStorage()),
-          ChangeNotifierProvider(create: (c)=>store.SuccStorage()),
+          ChangeNotifierProvider(create: (c)=>ReviewStorage()),
+          ChangeNotifierProvider(create: (c)=>SuccStorage()),
         ],
       child: GetMaterialApp(
         theme: style.theme,
-        home: MyApp(),
+        home: const MyApp(),
         getPages: [
           // GetPage(name: '/home', page: ()=> home.Page()),
           // GetPage(name: '/bookmark', page: ()=> bookmark.Page()),
@@ -62,9 +78,9 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    context.read<store.ReviewStorage>().getReviews();
-    context.read<store.SuccStorage>().getArticles();
-    context.read<store.ReviewStorage>().getMyReviews();
+    context.read<ReviewStorage>().getReviews();
+    context.read<SuccStorage>().getArticles();
+    context.read<ReviewStorage>().getMyReviews();
   }
   // 여기까지 함수 등
   @override
